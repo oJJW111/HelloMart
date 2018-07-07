e3951b0aeb91fb3deac9c466baa466b1f7931216
unknown <deter364@gmail.com>
2018-07-07 23:35:22 +0900

    hello

--- HelloMart/src/main/java/com/hellomart/dao/ProductDAO.java
+++
@@ -1,7 +0,0 @@
-package com.hellomart.dao;
-
-import com.hellomart.dto.ProductList;
-
-public interface ProductDAO {
-	ProductList getProductInfo(String no);
-}
--- HelloMart/src/main/java/com/hellomart/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.hellomart.service;
-
-import org.springframework.ui.Model;
-
-public interface ProductService {
-
-	void getProductInfo(String no, Model model);
- 
-}
